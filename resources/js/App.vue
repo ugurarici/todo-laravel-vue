ef79d91eebdc75bdfd7d711002500a772efa84e7
Uğur Arıcı <m.ugurarici@gmail.com>
2022-03-20 11:02:29 +0300

    Seperate TodoItem and create new todo form

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,14 +1,47 @@
 <template>
-  <div>App.vue {{ message }}</div>
+  <div>
+    App.vue {{ message }}
+    <div>
+      <ul>
+        <todo-item v-for="(todo, key) in todos" :key="key" :todo="todo" />
+      </ul>
+    </div>
+    <hr />
+    <form @submit.prevent="addTodo">
+      <input type="text" v-model="newTodo" autofocus />
+      <button type="submit">Add</button>
+    </form>
+  </div>
 </template>
 
 <script>
+import TodoItem from "./Todo/TodoItem.vue";
+
 export default {
   name: "App",
+  components: {
+    TodoItem,
+  },
   data() {
     return {
       message: "Hello Vue!",
+      newTodo: "",
+      todos: [
+        { text: "Learn JavaScript", done: true },
+        { text: "Learn Vue", done: false },
+        { text: "Build something awesome", done: false },
+      ],
     };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo == "") return;
+      this.todos.push({
+        text: this.newTodo,
+        done: false,
+      });
+      this.newTodo = "";
+    },
   },
 };
 </script>
---
+++ resources/js/Todo/TodoItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <li>
+    <label>
+      <input type="checkbox" v-model="todo.done" />
+      {{ todo.text }}
+    </label>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["todo"],
+};
+</script>
